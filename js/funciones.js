1842341b759bd32d5a91c13ce058bb8a75afa8a9
Federico <fede1985.mosqueira@gmail.com>
2016-09-03 16:21:58 -0300

    Detalles finales
    detalles finales de la web

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
-    
     animatedAnchor('.btn_anchor');
     showOverlay('#btn_contact', '.overlay_contact');
     showBtnFilter('#filter_works');
     fixedFilter();
-    // toggleMenu('#btn_toggleMenu', '#main_menu');
 
-    //inicializa reveal animation
     wow = new WOW(
       {
         animateClass: 'animated',
@@ -17,20 +14,24 @@ $(document).ready(function() {
       }
     );
     wow.init();
-});
 
+    $(function() {  
+        jQuery.scrollSpeed(100, 1000);
+    });
+
+});
 
 
 function animatedAnchor(btnSlide) {
     $(btnSlide).each(function(){
         $(this).on('click', function() {
-            event.preventDefault()
             var elementID = $(this).attr('href');
             var scrollLength = $(elementID).offset().top;
             
             $('html, body').animate({
                 scrollTop : scrollLength
             }, 1300, 'easeOutQuart');
+
         });
     });
 }
@@ -40,24 +41,18 @@ function showBtnFilter(conBtnFilter) {
         var positionWindow = $(window).scrollTop();
         var showBtnfilter = $('#header').outerHeight();
         var sizeHideElement = $(conBtnFilter).outerHeight();
-
         $(conBtnFilter).css('bottom', -sizeHideElement);
-            
         if (positionWindow > showBtnfilter) {
             $(conBtnFilter).css('bottom', '0');
         } else {
             $(conBtnFilter).css('bottom', -sizeHideElement);
         } 
-    
         hideBtnWorks(positionWindow, showBtnfilter);
     });
  }
 
-
 function hideBtnWorks(positionWindow, hideBtnWorks) {   
-
     var sizeHideElement = $('.header_top').outerHeight();
-
     if(positionWindow > hideBtnWorks) {
         $('#btn_works').css({
             'position': 'relative',
@@ -72,76 +67,39 @@ function hideBtnWorks(positionWindow, hideBtnWorks) {
 }
 
 function fixedFilter() {
-    
     var windowWidth = $(window).width();
-    var tabletViwport = 1024;
-    
+    var tabletViewport = 1024;
     $(window).scroll(function() {
         var showBtnfilter = $('#header').outerHeight();
         var positionWindow = $(window).scrollTop();
-
-        if (windowWidth < tabletViwport && positionWindow > showBtnfilter) { 
+        if (windowWidth < tabletViewport && positionWindow > showBtnfilter) { 
             $('#filter_works').children('div').addClass('fixedFilter');
-            
         } else {
             $('#filter_works').children('div').removeClass('fixedFilter');
         }
-    });
-
-    adjustHeaderRsp(tabletViwport, windowWidth);    
+    });    
 }
-
-
-function adjustHeaderRsp(tabletViwport, windowWidth) {
-    console.log(tabletViwport);
-    
-    var btnHeight = $('.btn-down').outerHeight();
-    var headerHeight = $('#main_menu').outerHeight();
-
-    if (windowWidth < tabletViwport) {
-        $('#main_menu').css('height', (headerHeight - btnHeight)); 
-    }
-
-} 
-
 
 function showOverlay(btnShow, overlay) {
-    var newOverlayWidth = $('#main_menu').outerWidth();
-    var setOverlayWidth = $(overlay).width(newOverlayWidth);
     var getOverlayWidth = $(overlay).width();
-
-    $(overlay).css('left', -getOverlayWidth);
+    var tabletViewport = 1024;
+    var windowWidth = $(window).width();
     
     $(btnShow).on('click', function() {
-        event.preventDefault();
+        var newOverlayWidth = $('#main_menu').outerWidth();
+        $(overlay).width(newOverlayWidth);
         $(overlay).css('left', '0');
+        hideOverlayResize(overlay, newOverlayWidth);
+        return false;
     });
-
     $('#btn-close').on('click', function() {
-        event.preventDefault();
-        $(this).parent(overlay).css('left', -getOverlayWidth);
+        $(this).parent(overlay).css('left', -getOverlayWidth * 2);
+        return false;
     });
 }
 
-
-
-// function toggleMenu(btnToggleMenu, mainMenu) {
-
-//     var widthMenu = $(mainMenu).outerWidth();
-
-//     $(btnToggleMenu).on('click', function() {
-
-//         $(mainMenu).css({
-//             'left': -widthMenu,
-//             'width': '0'
-//         });
-        
-//         $('#section_content').css({
-//             'width': '100%',
-//             'left': '0'
-//         });
-//     });
-
-// }
-
-
+function hideOverlayResize(overlay, newOverlayWidth) {
+    $(window).resize(function() {
+        $(overlay).css('left', -newOverlayWidth * 2);
+    });    
+}
